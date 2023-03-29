0fea53fa234f183fcda1d0410e16a4f9decaf230
kevinjcorcoran <kjc100@pitt.edu>
2023-03-28 22:33:19 -0400

    Reformatting the app to use Flask locally. Adding predictions

--- main.py
+++ main.py
@@ -1,55 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Blueprint, render_template, request
 import os
-import numpy as np
-import tensorflow as tf
+from .predict import predict
 
-app = Flask(__name__)
+# Primary Directories
+basePath = os.path.abspath(os.path.dirname(__file__))
+template_dir = os.path.join(basePath, 'templates')
 
-static_dir = os.path.join(app.root_path, 'static/')
-
-model_file = os.path.join(app.root_path, 'model/breed.h5')
-model = tf.keras.models.load_model(model_file)
-class_names = ['Afghan_hound', 'African_hunting_dog', 'Airedale', 'American_Staffordshire_terrier', 'Appenzeller', 'Australian_terrier', 'Bedlington_terrier', 'Bernese_mountain_dog', 'Blenheim_spaniel', 'Border_collie', 'Border_terrier', 'Boston_bull', 'Bouvier_des_Flandres', 'Brabancon_griffon', 'Brittany_spaniel', 'Cardigan', 'Chesapeake_Bay_retriever', 'Chihuahua', 'Dandie_Dinmont', 'Doberman', 'English_foxhound', 'English_setter', 'English_springer', 'EntleBucher', 'Eskimo_dog', 'French_bulldog', 'German_shepherd', 'German_short-haired_pointer', 'Gordon_setter', 'Great_Dane', 'Great_Pyrenees', 'Greater_Swiss_Mountain_dog', 'Ibizan_hound', 'Irish_setter', 'Irish_terrier', 'Irish_water_spaniel', 'Irish_wolfhound', 'Italian_greyhound', 'Japanese_spaniel', 'Kerry_blue_terrier', 'Labrador_retriever', 'Lakeland_terrier', 'Leonberg', 'Lhasa', 'Maltese_dog', 'Mexican_hairless', 'Newfoundland', 'Norfolk_terrier', 'Norwegian_elkhound', 'Norwich_terrier', 'Old_English_sheepdog', 'Pekinese', 'Pembroke', 'Pomeranian', 'Rhodesian_ridgeback', 'Rottweiler', 'Saint_Bernard', 'Saluki', 'Samoyed', 'Scotch_terrier', 'Scottish_deerhound', 'Sealyham_terrier', 'Shetland_sheepdog', 'Shih-Tzu', 'Siberian_husky', 'Staffordshire_bullterrier', 'Sussex_spaniel', 'Tibetan_mastiff', 'Tibetan_terrier', 'Walker_hound', 'Weimaraner', 'Welsh_springer_spaniel', 'West_Highland_white_terrier', 'Yorkshire_terrier', 'affenpinscher', 'basenji', 'basset', 'beagle', 'black-and-tan_coonhound', 'bloodhound', 'bluetick', 'borzoi', 'boxer', 'briard', 'bull_mastiff', 'cairn', 'chow', 'clumber', 'cocker_spaniel', 'collie', 'curly-coated_retriever', 'dhole', 'dingo', 'flat-coated_retriever', 'giant_schnauzer', 'golden_retriever', 'groenendael', 'keeshond', 'kelpie', 'komondor', 'kuvasz', 'malamute', 'malinois', 'miniature_pinscher', 'miniature_poodle', 'miniature_schnauzer', 'otterhound', 'papillon', 'pug', 'redbone', 'schipperke', 'silky_terrier', 'soft-coated_wheaten_terrier', 'standard_poodle', 'standard_schnauzer', 'toy_poodle', 'toy_terrier', 'vizsla', 'whippet', 'wire-haired_fox_terrier']
+main = Blueprint('main', __name__)
 
 
-@app.route('/')
-@app.route('/index.html') 
+@main.route('/')
+@main.route('/index.html') 
 def root():
     return render_template('index.html', page_title='Home')
 
 
-@app.route('/about.html')
+@main.route('/about.html')
 def about():
     return render_template('about.html', page_title='About')
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
+@main.route('/upload_and_predict', methods=['GET', 'POST'])
+def upload_and_predict():
+    target = os.path.join(basePath, 'temp/')
     if request.method == 'POST':
-        file = request.files['user-image']
-        file_name = file.filename
-        file_path = ''.join([static_dir, file_name])
-        file.save(file_path)
-        predict(file_path)
+        user_image = request.files['user-image']
+        file_name = user_image.filename
+        file_path = ''.join([target, file_name])
+        user_image.save(file_path)
 
+        results = predict(file_path)
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict(file_path):
-    img_height = 180
-    img_width = 180
+        os.remove(file_path)
 
-    img = tf.keras.utils.load_img(file_path, target_size=(img_height, img_width)) 
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)
-
-    predictions = model.predict(img_array)
-    score = tf.nn.softmax(predictions[0])
-
-    breed = class_names[np.argmax(score)]
-    precision = 100*np.max(score)
-
-    return render_template("results.html", page_title="Results", breed=breed, precision=precision)
-
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+        return render_template("results.html", page_title="Results", breed=results['breed'], precision=results['precision'])
