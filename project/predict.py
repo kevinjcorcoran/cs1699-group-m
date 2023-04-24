9652df3f0c51b923c2d79ef50a139bdf4226a63d
kevinjcorcoran <kjc100@pitt.edu>
2023-04-23 20:35:51 -0400

    Fixing class_names in predict.py

--- project/predict.py
+++ project/predict.py
@@ -9,10 +9,31 @@ template_dir = os.path.join(basePath, 'templates')
 static_dir = os.path.join(basePath, 'static/')
 
 model_file = os.path.join(basePath, 'model/eightyfiveepoch_512batch_breed.h5')
-model = tf.keras.models.load_model(model_file, compile=False)
-model.compile()
+model = tf.keras.models.load_model(model_file, compile=True)
 
-class_names = ['affenpinscher', 'Afghan_hound', 'African_hunting_dog', 'Airedale', 'american_pit_bull_terrier', 'American_Staffordshire_terrier', 'basenji', 'basset', 'beagle', 'Bernese_mountain_dog', 'Blenheim_spanial', 'bloodhound', 'bluetick', 'borzoi', 'Boston_bull', 'Bouvier_des_Flandres', 'boxer', 'briard', 'Brittany_spaniel', 'bull_mastiff', 'cairn', 'Cardigan', 'Chihuahua', 'chow', 'clumber', 'cocker_spaniel', 'collie', 'dhole', 'dingo', 'Doberman', 'English_foxhound', 'English_setter', 'English_springer', 'French_bulldog', 'German_shepherd', 'German_short-haired_pointer', 'golden_retriever', 'Gordon_setter', 'Great_Dane', 'Great_Pyrenees', 'Greater_Swiss_Mountain_dog', 'groenendael', 'Irish_setter', 'Irish_water_spaniel', 'Irish_wolfhound', 'Italian_greyhound', 'Japanese_spaniel', 'keeshond', 'kelpie', 'komondor', 'kuvasz', 'Labrador_retriever', 'Lakeland_terrier', 'Leonberg', 'Lhasa', 'malamute', 'malinois', 'Maltese_dog', 'Mexican_hairless', 'miniature_pinscher', 'Newfoundland', 'Norwegian_elkhound', 'Old_English_sheepdog', 'papillon', 'Pekinese', 'Pembroke', 'Pomeranian', 'pug', 'Rhodesian_ridgeback', 'Rottweiler', 'Saint_Bernard', 'Samoyed', 'schipperke', 'Scotch_terrier', 'Scottish_deerhound', 'Shetland_sheepdog', 'shiba_inu', 'Shih-Tzu', 'Siberian_husky', 'Staffordshire_bullterrier', 'standard_poodle', 'standard_schnauzer', 'Sussex_spanial', 'Tibetan_mastiff', 'toy_poodle', 'toy_terrier', 'vizsla', 'Weimaraner', 'Welsh_springer_spaniel', 'wire-haired_fox_terrier', 'Yorkshire_terrier']
+class_names = ['Afghan_hound', 'African_hunting_dog', 'Airedale',
+    'American_Staffordshire_terrier', 'Bernese_mountain_dog', 'Blenheim_spaniel',
+    'Boston_bull', 'Bouvier_des_Flandres', 'Brittany_spaniel', 'Cardigan',
+    'Chihuahua', 'Doberman', 'English_foxhound', 'English_setter',
+    'English_springer', 'French_bulldog', 'German_shepherd',
+    'German_short-haired_pointer', 'Gordon_setter', 'Great_Dane',
+    'Great_Pyrenees', 'Greater_Swiss_Mountain_dog', 'Irish_setter',
+    'Irish_water_spaniel', 'Irish_wolfhound', 'Italian_greyhound',
+    'Japanese_spaniel', 'Labrador_retriever', 'Lakeland_terrier', 'Leonberg',
+    'Lhasa', 'Maltese_dog', 'Mexican_hairless', 'Newfoundland',
+    'Norwegian_elkhound', 'Old_English_sheepdog', 'Pekinese', 'Pembroke',
+    'Pomeranian', 'Rhodesian_ridgeback', 'Rottweiler', 'Saint_Bernard', 'Samoyed',
+    'Scotch_terrier', 'Scottish_deerhound', 'Shetland_sheepdog', 'Shih-Tzu',
+    'Siberian_husky', 'Staffordshire_bullterrier', 'Sussex_spaniel',
+    'Tibetan_mastiff', 'Weimaraner', 'Welsh_springer_spaniel',
+    'Yorkshire_terrier', 'affenpinscher', 'american_pit_bull_terrier', 'basenji',
+    'basset', 'beagle', 'bloodhound', 'bluetick', 'borzoi', 'boxer', 'briard',
+    'bull_mastiff', 'cairn', 'chow', 'clumber', 'cocker_spaniel', 'collie',
+    'dhole', 'dingo', 'golden_retriever', 'groenendael', 'keeshond', 'kelpie',
+    'komondor', 'kuvasz', 'malamute', 'malinois', 'miniature_pinscher',
+    'papillon', 'pug', 'schipperke', 'shiba_inu', 'standard_poodle',
+    'standard_schnauzer', 'toy_poodle', 'toy_terrier', 'vizsla',
+    'wire-haired_fox_terrier']
 
 
 def predict(file_path):
